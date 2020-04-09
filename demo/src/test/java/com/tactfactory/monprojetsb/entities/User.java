d4912c2210dfd37c9722e471a5951c02bbc0ad2e
Emilien <emilien.gantois.pro@gmail.com>
2020-04-09 11:18:11 +0200

    add entities

---
+++ demo/src/test/java/com/tactfactory/monprojetsb/entities/User.java
@@ -0,0 +1,57 @@
+package com.tactfactory.monprojetsb.entities;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String firstname;
+	private String lastname;
+	
+	@OneToMany
+	private List<Product> products;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public User(Long id, String firstname, String lastname, List<Product> products) {
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.products = products;
+	}
+}
