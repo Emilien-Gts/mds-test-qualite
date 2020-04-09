9fe10650f04ae9f8cff748a33db4589e620c75b8
Emilien <emilien.gantois.pro@gmail.com>
2020-04-09 11:50:16 +0200

    add controllers and repository

---
+++ demo/src/test/java/com/tactfactory/monprojetsb/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.tactfactory.monprojetsb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tactfactory.monprojetsb.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
+
